af9d7e2e3c5067a20c2df82b4fb0fd9acd8af6c4
Deloweb <lucien.delohen@gmail.com>
2020-06-28 16:16:58 +0200

    Optimized code

--- src/CommentList.js
+++ src/CommentList.js
@@ -4,53 +4,32 @@ class CommentList extends Component {
   state = {};
 
   render() {
-    let commentsList = this.props.comments.map((comment) => {
-      let isadmin = this.props.isadmin ? (
-        <article className="media">
-          <div className="media-content">
-            <div className="content">
-              <p key={comment.id}>
-                <strong>{comment.name}</strong> <br />
-                {comment.message}
-              </p>
-            </div>
-          </div>
-          <button
-            className="delete"
-            onClick={() => this.props.handleDelete(comment.id)}
-          >
-            X
-          </button>
-        </article>
-      ) : (
-        <article className="media">
-          <div className="media-content">
-            <div className="content">
-              <p key={comment.id}>
-                <strong>{comment.name}</strong> <br />
-                {comment.message}
-              </p>
-            </div>
-          </div>
-        </article>
-      );
-
-      return <div>{isadmin}</div>;
-    });
-
-    let commentsCounter = this.props.comments.length;
-
     return (
       <div className="Commentlist">
         <h2 className="title is-h2">
-          Liste des commentaires ({commentsCounter})
+          Liste des commentaires ({this.props.comments.length})
         </h2>
 
-        <article className="media">
-          <div className="media-content">
-            <div className="content">{commentsList}</div>
-          </div>
-        </article>
+        {this.props.comments.map((comment) => (
+          <article className="media">
+            <div className="media-content">
+              <div className="content">
+                <p key={comment.id}>
+                  <strong>{comment.name}</strong> <br />
+                  {comment.message}
+                </p>
+              </div>
+            </div>
+            {this.props.isadmin && (
+              <button
+                className="delete"
+                onClick={() => this.props.handleDelete(comment.id)}
+              >
+                X
+              </button>
+            )}
+          </article>
+        ))}
       </div>
     );
   }
